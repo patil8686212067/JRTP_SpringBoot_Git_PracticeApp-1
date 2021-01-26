3340deb543c3b024d518d4be826c0d7cefd3646a
anil <anilrspatil1992@gmail.com>
2021-01-26 21:21:05 +0530

    save empty project to local

---
+++ src/main/java/com/ashokit/GitCommandPracticeApp1Application.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandPracticeApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommandPracticeApp1Application.class, args);
+	}
+
+}
