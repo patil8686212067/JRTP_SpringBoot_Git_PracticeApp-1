aae88950980c03bad655329b1ba535a22fc83738
anil <anilrspatil1992@gmail.com>
2021-01-26 22:12:32 +0530

    add one more file

---
+++ src/main/java/com/ashokit/client/TestForStashCmd.java
@@ -0,0 +1,13 @@
+package com.ashokit.client;
+
+public class TestForStashCmd {
+
+	public static void main(String[] args) {
+
+		
+		//---------HIS-122 is my current JIRA Task  i am working on it now at 8 am----
+		
+		System.out.println("----HIS-122---------");
+	}
+
+}
